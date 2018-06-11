a70447686fabf2b92fcd801e85f7bf4e464bfc7f
GROOVETECH\cuonglam <cuong.lam@groovetechnology.com>
2018-06-11 11:47:26 +0700

    aaa

--- StudyDocker/Program.cs
+++ StudyDocker/Program.cs
@@ -6,7 +6,7 @@ namespace StudyDocker
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!  ");
             Console.Read();
         }
     }
